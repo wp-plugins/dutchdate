a5f494bad4ef791ceac4b83c7c5dfd3e9dfb57e3
mendezki <mendezki@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-28 11:52:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/dutchdate/trunk@20295 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mp_dutchdate.php
+++ mp_dutchdate.php
@@ -3,7 +3,7 @@
 Plugin Name: DutchDate
 Plugin URI: http://www.multipop.org/dutchdate
 Description: Automagically converts english months and days (except for the initials) to their dutch equivalent.
-Version: 1.1
+Version: 1.2
 Author: Barry Ceelen
 Author URI: http://www.multipop.org
 
@@ -25,8 +25,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 function multipop_date_replace($text) {
-	$days_english = array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Mar', 'May', 'Oct', 'January', 'February', 'March', 'May', 'June', 'July', 'October');
-	$days_dutch = array('Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'Zo', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag', 'Mrt', 'Mei', 'Okt', 'Januari', 'Februari', 'Maart', 'Mei', 'Juni', 'Juli', 'Oktober');
+	$days_english = array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Mar', 'May', 'Oct', 'January', 'February', 'March', 'May', 'June', 'July', 'August', 'October', 'nd', 'th');
+	$days_dutch = array('Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'Zo', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag', 'Mrt', 'Mei', 'Okt', 'Januari', 'Februari', 'Maart', 'Mei', 'Juni', 'Juli', 'Augustus', 'Oktober', '', '');
 	$text = str_replace($days_english, $days_dutch , $text);
 	return $text;
 }
--- readme.txt
+++ readme.txt
@@ -1,5 +1,10 @@
-=== DutchDate ===Contributors: mendezkiTags: dutch, english, date, translateRequires at least: 1.5Tested up to: 2.2.2Stable tag: 1.1
+=== DutchDate ===Contributors: mendezkiTags: dutch, english, date, translateRequires at least: 1.5Tested up to: 2.2.2Stable tag: 1.2
 
 Replaces english day and month names with their dutch equivalent == Description ==Using an English Wordpress install and publishing posts in Dutch? Those pesky English dates produced by `<?php the_time(); ?>` were getting on my nerves too, hence this plugin. Automagically converts english months and days (except for the initials, so far) to their dutch equivalent.== Installation ==1. Copy the mp_dutchdate.php file to your Wordpress plugin directory.
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. There you go!
+
+== Changes ==
+
+Previously omitted August > Augustus, corrected in this version.
+Now also removes nd and th.
