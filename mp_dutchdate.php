2448b978f96ab817297734625ea2067f5d57cf6e
mendezki <mendezki@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-06 12:39:10 +0000

    Added more filters
    git-svn-id: https://plugins.svn.wordpress.org/dutchdate/trunk@752266 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mp_dutchdate.php
+++ mp_dutchdate.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: DutchDate
 Plugin URI: http://multipop.org/dutchdate
-Description: Automagically converts english months and days (except for the initials) to their dutch equivalent.
-Version: 1.6
+Description: Translates english months and days output from WordPress template tags to their dutch equivalent.
+Version: 1.8
 Author: Barry Ceelen
 Author URI: http://multipop.org
 
@@ -22,26 +22,30 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-function multipop_date_replace($text) {
-	$days_english = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
-	'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 
-	'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 
-	'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 
-	'1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '0th');
-	
-	$days_dutch = array('Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag', 
-	'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'Zo', 
-	'januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december', 
-	'jan', 'feb', 'mrt', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec',
-	'1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
-	$text = str_replace($days_english, $days_dutch, $text);
-	return $text;
+function multipop_date_replace( $string ) {
+	$english = array(
+		'Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+		'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun',
+		'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
+		'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+		'1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '0th'
+	);
+	$dutch = array(
+		'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag',
+		'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'Zo',
+		'januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december',
+		'jan', 'feb', 'mrt', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec',
+		'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+	);
+	$string = str_replace( $english, $dutch, $string );
+	return $string;
 }
 
 add_filter('the_time', 'multipop_date_replace');
-add_filter('the_modified_time', 'multipop_date_replace');
 add_filter('the_date', 'multipop_date_replace');
+add_filter('get_the_time', 'multipop_date_replace');
+add_filter('get_the_date', 'multipop_date_replace');
+add_filter('the_modified_time', 'multipop_date_replace');
 add_filter('the_modified_date', 'multipop_date_replace');
-add_filter('get_comment_date','multipop_date_replace');
-
-?>
+add_filter('get_the_modified_time', 'multipop_date_replace');
+add_filter('get_the_modified_date', 'multipop_date_replace')
